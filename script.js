ae1a93fe73c7b1b75f629e286738eae9d58c7811
nazar_yuriichuk <nazarurijcuk76@gmail.com>
2020-07-20 22:44:12 +0300

    second commit

--- script.js
+++ script.js
@@ -1,67 +1,18 @@
-let money = +prompt("Ваш бюджет на місяць ?");
-let time = prompt("Введіть дату в форматі YYYY-MM-DD");
+"use script";
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    income: [],
-    optionalExpenses: {},
-    savings: false
+let numbersOfFilms = +prompt("Enter your count of watching films ");
+
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
 };
 
 
-
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Ввеідть обов'язкову статтю витрат в цьому місяці");
-    let b = prompt("Сума?");
-
-    if ((typeof (a)) === 'string' && (typeof (a)) !== null && (typeof (b)) !== b &&
-        a !== '' && b !== '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad");
-        i--;
-    }
-}
-
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt("Ввеідть обов'язкову статтю витрат в цьому місяці");
-//     let b = prompt("Сума?");
-
-//     if ((typeof (a)) === "string" && !a && !b && a.length < 50) {
-//         console.log("done");
-//         appData.expenses[a] = b;
-//     }
-//     i++;
-// }
-
-// do {
-//     let a = prompt("Ввеідть обов'язкову статтю витрат в цьому місяці");
-//     let b = prompt("Сума?");
-
-//     if ((typeof (a)) === 'string' && (typeof (a)) !== null && (typeof (b)) !== b &&
-//         a !== '' && b !== '' && a.length < 50) {
-//         console.log("done");
-//         appData.expenses[a] = b;
-//     }
-//     i++;
-// } while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Щоденний бюджет " + appData.moneyPerDay);
-
-
-if (appData.moneyPerDay < 1000) {
-    console.log("Мінінмальний рівень достатку");
-} else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 2000) {
-    console.log("Середній рівень достатку");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Високий рівень достатку");
-} else {
-    console.log("Помилка");
+for (let i = 1; i <= 2; i++) {
+    let film = prompt("Your last watching film");
+    let value = +prompt("Your rating", 0);
+    personalMovieDB.movies[film] = value;
 }
